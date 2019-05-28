2158866a83dedc915a968a648836f22af3fa2cf9
miraclx <omiraculous@gmail.com>
2019-05-28 01:46:48 +0100

    Upped to v0.2.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="xstrip_auth",
-    version="0.2.0",
+    version="0.2.1",
     author="Miraculous Owonubi",
     author_email="omiraculous@gmail.com",
     description="Cryptographically strong pseudorandom key generator based on the XStrip Algorithm",
